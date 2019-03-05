a8915de387873ac93ad23660691610ae9377b3cb
Lauren Blake <lmb737@nyu.edu>
2019-03-05 12:28:37 -0500

    Beginning Stages
    Arrow Keys/highlighting responses works. Questions + Responses are staged. (NEED TO DO: Changing of Sprite + Scoring system)

---
+++ Cat__Roommate/Assets/Scripts/ChangeQuestion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeQuestion : MonoBehaviour
+{
+    public TextMeshProUGUI _catQuestions;
+    public string[] _allQuestions;
+    private int _currentQuestion = 0;
+    private int _totalQuestions;
+
+    //Change the Response/Question that the player can choose
+    public void changeQuestion()
+    {
+
+        _totalQuestions = _allQuestions.Length;
+
+        if(_currentQuestion < _totalQuestions){
+            _catQuestions.text = _allQuestions[_currentQuestion];
+        }
+        _currentQuestion++;
+
+
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Cat__Roommate/Assets/Scripts/PlayerResponse.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerResponse : MonoBehaviour
+{
+    public bool _leftResponse;
+    public bool _rightResponse;
+    public TextMeshProUGUI _rightResponsetxt;
+    public TextMeshProUGUI _leftResponsetxt;
+    public GameObject CatRoommateQuestions;
+    public GameObject Response2;
+    public GameObject Response1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hello please work");
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            _rightResponse = true;
+            _leftResponse = false;
+            _rightResponsetxt.color = new Color(0, 0, 0,100);
+            _leftResponsetxt.color = new Color(255, 255, 255, 100);
+            Debug.Log("The Color should be black on the right");
+        }
+
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            _leftResponse = true;
+            _rightResponse = false;
+            _leftResponsetxt.color = new Color(0, 0, 0, 100);
+            _rightResponsetxt.color = new Color(255, 255, 255, 100);
+            Debug.Log("The Color should be black on the left");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("NEXT QUESTIONS");
+            Response1.GetComponent<ChangeQuestion>().changeQuestion();
+            Response2.GetComponent<ChangeQuestion>().changeQuestion();
+            CatRoommateQuestions.GetComponent<ChangeQuestion>().changeQuestion();
+
+        }
+    }
+}
